58c5d115b8bd4b42bb6307ecf9b9a386e602fde2
Navraj <navrajmann138@gmail.com>
2025-11-11 13:26:23 +1300

    feat : added zen mode

---
+++ lua/plugins/ui/zen.lua
@@ -0,0 +1,42 @@
+return {
+    "folke/zen-mode.nvim",
+    opts = {
+        window = {
+            backdrop = 1,
+            width = 0.5,
+            height = .95,
+            options = {
+                number = false,
+                relativenumber = false,
+            }
+        },
+        plugins = {
+            options = {
+                enabled = true,
+                laststatus = 0
+            },
+            kitty = {
+                enabled = true,
+                font = "+2",
+            }
+        },
+        on_open = function()
+            -- disable Snacks indent guides
+            if package.loaded["snacks"] then
+                require("snacks").indent.disable()
+            end
+        end,
+        on_close = function()
+            -- re-enable Snacks indent guides
+            if package.loaded["snacks"] then
+                require("snacks").indent.enable()
+            end
+        end,
+    },
+    config = function(_, opts)
+        require("zen-mode").setup(opts)
+        vim.keymap.set("n", "<leader>z", function()
+            require("zen-mode").toggle()
+        end, { desc = "Toggle Zen Mode" })
+    end,
+}
