5a609972dc1dff9b3aa1f07a3c5b872701e7dd9e
Navraj <navrajmann138@gmail.com>
2025-11-12 17:42:25 +1300

    feat : added themery for persistient themes and an easy theme swapper

--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -1,10 +1,47 @@
 return {
+    -- themery
+    {
+        "zaldih/themery.nvim",
+        lazy = false,
+        config = function()
+            require("themery").setup({
+                themes = {
+                    -- rose-pine
+                    { name = "rose-pine",            colorscheme = "rose-pine" },
+                    { name = "rose-pine-moon",       colorscheme = "rose-pine-moon" },
+
+                    -- catppuccin
+                    { name = "catppuccin-frappe",    colorscheme = "catppuccin-frappe" },
+                    { name = "catppuccin-macchiato", colorscheme = "catppuccin-macchiato" },
+                    { name = "catppuccin-mocha",     colorscheme = "catppuccin-mocha" },
+
+                    -- tokyonight
+                    { name = "tokyo-night-storm",    colorscheme = "tokyonight-storm" },
+                    { name = "tokyo-night-night",    colorscheme = "tokyonight-night" },
+
+                    -- lavi
+                    { name = "lavi",                 colorscheme = "lavi" },
+
+                    -- nordic
+                    { name = "nordic",               colorscheme = "nordic" },
+
+                    -- night own
+                    { name = "night-owl",            colorscheme = "night-owl" },
+                },
+                livePreview = true,
+            })
+
+            -- Keymap: <leader>c to open Themery
+            vim.keymap.set("n", "<leader>c", "<cmd>Themery<CR>", { desc = "Open Themery theme picker" })
+        end,
+    },
+
     -- rose pine
     {
         "rose-pine/neovim",
         name = "rose-pine",
         priority = 1000,
-        lazy = false,
+        lazy = true,
     },
 
     -- catppuccin
@@ -12,7 +49,7 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        lazy = false,
+        lazy = true,
     },
 
     -- tokyo night
@@ -20,25 +57,36 @@ return {
         "folke/tokyonight.nvim",
         name = "tokyo-night",
         priority = 1000,
-        lazy = false,
-    },
-
-    -- github
-    {
-        "projekt0n/github-nvim-theme",
-        name = "github",
-        priority = 1000,
-        lazy = false,
-
+        lazy = true,
     },
 
     -- lavi
     {
         "b0o/lavi.nvim",
+        name = "lavi",
         dependencies = { "rktjmp/lush.nvim" },
         config = function()
             vim.cmd("colorscheme lavi")
         end,
+        lazy = true,
     },
 
+    -- nordic
+    {
+        'AlexvZyl/nordic.nvim',
+        name = "nordic",
+        lazy = true,
+        priority = 1000,
+        config = function()
+            require('nordic').load()
+        end
+    },
+
+    -- night owl
+    {
+        "oxfist/night-owl.nvim",
+        name = "night-owl",
+        lazy = true,
+        priority = 1000,
+    },
 }
